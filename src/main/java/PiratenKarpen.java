f25b3231ae62d02f4a6a8df294bd0dfde12963de
zgarada <zgarada2003@gmail.com>
2023-01-17 22:31:55 -0500

    Created new class 'Player' to create objects that can store number of dice, score, and other such things more easily by creating objects.

--- src/main/java/PiratenKarpen.java
+++ src/main/java/PiratenKarpen.java
@@ -1,23 +1,15 @@
 import pk.Dice;
 import pk.Faces;
+import pk.Player;
 
 public class PiratenKarpen {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Piraten Karpen Simulator!");
         System.out.println("I'm rolling the dice");
-        Dice myDice = new Dice();
-        int skulls = 0;
-        int dice = 9;
-        while (skulls<3){
-            System.out.println(myDice.roll());
-            if (myDice.roll() == Faces.SKULL){
-                skulls++;
-                dice--;
-            }
-            if (myDice.roll() == Faces.GOLD || myDice.roll() == Faces.DIAMOND)
-        }
-            System.out.println(myDice.roll());
+        Player player1 = new Player();
+        
+        
         System.out.println("That's all folks!");
     }
     
---
+++ src/main/java/pk/Player.java
@@ -0,0 +1,13 @@
+package pk;
+import pk.Faces;
+import pk.Dice;
+
+public class Player {
+    static int score;
+    static int skulls;
+    static Dice myDice = new Dice();
+}
+    
+
+    
+
