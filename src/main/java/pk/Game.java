2762c7d27fbb430cf54c9a6f037be8a4a1d40561
zgarada <zgarada2003@gmail.com>
2023-01-25 14:19:35 -0500

    Fixed error when the 2 players tie.

--- src/main/java/pk/Game.java
+++ src/main/java/pk/Game.java
@@ -8,66 +8,36 @@ public class Game {
         int first = start.nextInt(2);
         while (p1.result == false && p2.result == false){
             if (first == 0){
-            Roll.turn(p1);
-            if (Roll.turn(p1) == false){
+                Roll.turn(p1);
                 Roll.turn(p2);
-            }
             }
             else{
                 Roll.turn(p2);
-                if (Roll.turn(p2) == false){
-                    Roll.turn(p1);
-                }
+                Roll.turn(p1);
             }
         }
-        if (p1.result == true && first == 0){
-            Roll.turn(p2);
-            if (p1.score > p2.score) {
-                //System.out.println("Player 1 wins!");
-                p1.num_wins++;
-            }
-            else if (p1.score == p2.score){
-                Roll.turn(p1); Roll.turn(p2);
-                if (p1.score > p2.score) {
-                    //System.out.println("Player 1 wins!");
-                    p1.num_wins++;
-                }
-                else {
-                    //System.out.println("Player 2 wins!");
-                    p2.num_wins++;
-                }
-            }
-            else {
-                //System.out.println("Player 2 wins!");
-                p2.num_wins++;
-            }
-            //System.out.println(p1.score);
-        }
-        //if p2 starts
-        if (p2.result == true && first == 1){
-            Roll.turn(p1);
-            if (p2.score>p1.score){
-               // System.out.println("Player 2 wins!");
-                p2.num_wins++;
-            }
-            else if (p1.score == p2.score){
-                Roll.turn(p2);Roll.turn(p1);
-                if (p1.score > p2.score) {
-                  //  System.out.println("Player 1 wins!");
-                    p1.num_wins++;
-                }
-                else {
-                   // System.out.println("Player 2 wins!");
-                    p2.num_wins++;
-                }
-            }
-            else{
-               // System.out.println("Player 1 wins!");
-                p1.num_wins++;
-            }
 
 
-            //System.out.println(p2.score);
+        while(p1.score == p2.score){
+            Roll.turn(p1);Roll.turn(p2);
         }
+
+        if (p1.score>p2.score){
+            p1.num_wins++;
+            System.out.println("Player 1 score: " + p1.score);System.out.println("Player 2 score: " + p2.score);
+            System.out.println("Player 1 wins!");
+        }
+        else if (p1.score<p2.score){
+            p2.num_wins++;
+            System.out.println("Player 1 score: " + p1.score);System.out.println("Player 2 score: " + p2.score);
+            System.out.println("Player 2 wins!");
+        }
+
+        else{
+            System.out.println("Player 1 score: " + p1.score);System.out.println("Player 2 score: " + p2.score);
+            System.out.println("What da faq");
+        }
+
+
     }
 }
--- src/main/java/pk/Roll.java
+++ src/main/java/pk/Roll.java
@@ -3,8 +3,24 @@ import java.util.Random;
 public class Roll {
     public static boolean turn(Player p) {
         Dice myDice = new Dice();
+        //Initial roll of all 8 dice
+        for (int i = 0; i< 8 ; i++){
+            if (myDice.roll() == Faces.SKULL){
+                p.dice--;
+                p.skulls++;
+            }
+            if (myDice.roll() == Faces.GOLD || myDice.roll() == Faces.DIAMOND){
+                p.score += 100;
+            }
+            if (p.score >= 6000){
+                p.result = true;
+                return p.result;
+            }
+        }
+
         Random reroll = new Random(); Random random_numDice = new Random();
         int count = 0;
+
             while (p.skulls<3){ //Each roll
                 //Implement end turn at random
                 int reroll_int = reroll.nextInt(2);
@@ -12,7 +28,6 @@ public class Roll {
                 if (reroll_int == 1){
                     int randNumDice = random_numDice.nextInt(2,p.dice+1);
                     for (int i = 0; i<randNumDice;i++){ //Rolling the X dice
-
                         if (myDice.roll() == Faces.SKULL){
                             p.dice--;
                             p.skulls++;
@@ -29,11 +44,14 @@ public class Roll {
                 //If not reroll and end turn, add points to score
                 else{
                     p.score += count;
+                    if (p.score >= 6000){
+                        p.result = true;
+                        return p.result;
+                    }
                     break;
                 }
 
             }
-
         p.skulls = 0;
         p.dice = 8; 
         return p.result;
